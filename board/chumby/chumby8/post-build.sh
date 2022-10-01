5bcb56c3e380db9d8afe7e592840d2d4dcd1c285
Doug Brown <doug@schmorgal.com>
2022-09-30 20:23:22 -0700

    chumby8: add settings partition
    This will be used to store settings on a rewritable partition so we
    don't have to remount the rootfs read/write all the time when things are
    being changed.

---
+++ board/chumby/chumby8/post-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+
+# Make sure we have a directory to use as a mountpoint
+mkdir -p ${TARGET_DIR}/mnt/settings
+
+# Populate settings partition
+mkdir -p ${BINARIES_DIR}/settings
